310913e82611481fbefd776f19251c65f5afce33
asadu <asadu@MSI>
2024-04-23 20:56:46 -0400

    Add project files.

---
+++ Question4FinalTest2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Question4FinalTest2
+{
+    public class ValueCollection<T>where T : struct
+    {
+        private List<T> collection = new List<T>();
+        public void AddItem(T item)
+        {
+            collection.Add(item);
+        }
+        public T GetItem(int index) {
+            if (index <0||index>=collection.Count)
+            {
+                throw new IndexOutOfRangeException("index is out of range");
+            }
+            return collection[index];
+        }
+        public IEnumerable<T> GetSortedDescending() {
+            return collection.OrderByDescending(item => item);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
